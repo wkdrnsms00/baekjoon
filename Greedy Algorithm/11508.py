3d5cee21e031357c0ec0fc4e757a1d41063553ef
wkdrnsms00 <hold5732@naver.com>
2022-01-11 16:11:00 +0900

    수정

--- Greedy Algorithm/11508.py
+++ Greedy Algorithm/11508.py
@@ -1,22 +1,4 @@
 # 2+1 세일
-# N = int(input())
-# temp = []
-# res = 0
-# for i in range(N):
-#     temp.append(int(input()))
-
-# a = len(temp)//3
-# b = len(temp)%3
-
-# for i in range(a):
-#     res = res + sum(temp[0:3]) - min(temp[0:3])
-#     for j in range(3):
-#         temp.pop(0)
-# for i in range(b):
-#     res = res + temp[b]
-#     temp.pop[b]
-
-# print(res) 런타임 에러 코드
 
 N = int(input())
 temp = []
