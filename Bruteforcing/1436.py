aaad8fb99635aa2fa347ef7a9e2b55b04831f429
Wkdrnsms00 <hold5732@naver.com>
2022-03-23 18:39:07 +0900

    영화감독 숌

---
+++ Bruteforcing/1436.py
@@ -0,0 +1,12 @@
+in_value = int(input())
+out_value = 0
+end_str = 666
+
+while(1):
+    if '666' in str(end_str):
+        out_value +=1
+    if out_value == in_value:
+        print(end_str)
+        break
+    end_str +=1
+
