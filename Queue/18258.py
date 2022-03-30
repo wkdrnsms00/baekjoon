d59eb30ff46f3f4e9c0382814f4fffbf39bb7d55
Wkdrnsms00 <hold5732@naver.com>
2022-03-30 17:05:27 +0900

    큐2

---
+++ Queue/18258.py
@@ -0,0 +1,59 @@
+# 큐2
+from sys import stdin
+from collections import deque
+
+def push (list, num):
+    list.append(num)
+    return list
+
+def pop (list):
+    if len(list) == 0:
+        print('-1')
+    else:
+        print(list.popleft())
+    return list
+
+def size(list):
+    print(len(list))
+    return list
+
+def empty(list):
+    if len(list) == 0:
+        print('1')
+    else:
+        print('0')
+    return list
+
+def front(list):
+    if len(list) == 0:
+        print('-1')
+    else:
+        print(list[0])
+    return list
+
+def back(list):
+    if len(list) == 0:
+        print('-1')
+    else:
+        print(list[-1])
+    return list
+
+ 
+N = int(stdin.readline())
+data = deque([])
+for i in range(N):
+    Q = stdin.readline().split()
+    if Q[0] == 'push':
+        push(data, Q[1])
+    elif Q[0] == 'pop':
+        pop(data)
+    elif Q[0] == 'size':
+        size(data)
+    elif Q[0] == 'empty':
+        empty(data)
+    elif Q[0] == 'front':
+        front(data)
+    else:
+        back(data)
+    
+
